9be0df4a1073140365006d9d304fa6e31775a6e5
yinho999 <yinho999@gmail.com>
2022-03-10 09:31:00 +0000

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Fail to find random port");
+
+    run(listener)?.await
 }
