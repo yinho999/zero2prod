c4114908f6480831359e4617b124cd54061a11c2
yinho999 <yinho999@gmail.com>
2022-03-22 05:06:33 +0000

    clear all

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::net::TcpListener;
-use zero2prod::run;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Fail to find random port");
-
-    run(listener)?.await
-}
